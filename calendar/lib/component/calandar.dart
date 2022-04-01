eadc2cb5ceb47395844df80f7c00c2edbf5a9cbb
onMinyoung22 <minyoung_stat@dgu.ac.kr>
2022-04-01 22:09:23 +0900

    dwdqw

---
+++ calendar/lib/component/calandar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime? selectedDay;
+  
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1800),
+      lastDay: DateTime(3000),
+      headerStyle: HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+      ),
+      onDaySelected: (sel, foc) {
+        setState(() {
+          this.selectedDay = sel;
+        });
+      },
+      selectedDayPredicate: (date) {
+        if(selectedDay == null) return false;
+        
+        return date.year == selectedDay!.year &&
+        date.month == selectedDay!.month &&
+        date.day == selectedDay!.day;
+      },
+    );
+  }
+}
---
+++ calendar/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:calendar/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomeScreen()));
+}
+
